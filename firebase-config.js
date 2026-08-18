bad7227b177d64a04ca7aee1b5148e75b00c8847
Oggy3322 <Eshan32884332@gmail.com>
2026-08-18 14:46:14 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,28 +1,21 @@
-import {
-  initializeApp
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCfiqHTb3sGk1e7cfPyVe0oXsDJsna2Gbeo",
+  apiKey: "AIzaSyCfiqHTb3sGk1e7cfPyVeOoXsDJsm2Gbeo",
   authDomain: "water-price-top-up-bd.firebaseapp.com",
   projectId: "water-price-top-up-bd",
   storageBucket: "water-price-top-up-bd.firebasestorage.app",
   messagingSenderId: "759484315438",
-  appId: "1:759484315438:web:9630e5c17673b214526cbf",
+  appId: "1:759484315438:web:0630e5c17673b214526cbf",
   measurementId: "G-8252E0PJPM"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
